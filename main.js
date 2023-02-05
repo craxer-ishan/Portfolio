cd475efe0e123408402a2335b653b05939e7e42a
craxer-ishan <np03cs4a220191@heraldcollege.edu.np>
2023-02-05 17:32:13 +0545

    changes

--- main.js
+++ main.js
@@ -31,16 +31,51 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header')
 
+function toggleSkills(){
+    let itemClass = this.parentNode.className
 
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass == 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleSkills)
+})
 /*==================== QUALIFICATION TABS ====================*/
 
 
 /*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('services__button'),
+      modalCloses = document.querySelectorAll('.services__modal-close')
 
-
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+} 
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () =>{
+        modal(i)
+    })
+})
 /*==================== PORTFOLIO SWIPER  ====================*/
-
+var swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
 
 /*==================== TESTIMONIAL ====================*/
 
